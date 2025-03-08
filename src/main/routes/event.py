1c1915b52b3b668ff6a5c079bf52b26960cb0f56
wildemberg-sales <junior_sales2010@hotmail.com>
2025-03-08 15:39:06 -0300

    feat: implement http types

--- src/main/routes/event.py
+++ src/main/routes/event.py
@@ -1,11 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from datetime import datetime
+from src.http_types.http_response import HttpResponse
+from src.http_types.http_request import HttpRequest
 
 event_route = APIRouter(prefix="/events")
 
 @event_route.get("/")
-def get():
-    data = [{"message":"eu sou foda", "date": datetime.now()}, {"teste": "sou fodão", "date": datetime.now()}]
-    return JSONResponse(content=jsonable_encoder(data), status_code=200)
+async def get(request: Request):
+    http_request = HttpRequest(body=await request.json())
+    
+    http_response = HttpResponse(body=http_request.body, status_code=200)
+    
+    return JSONResponse(content=jsonable_encoder(http_response.body), status_code=http_response.status_code)
